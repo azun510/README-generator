3a17b164fca3c2b6929ff2609058bb8a91fdbaf9
Andrew Zuniga <azuniga183@gmail.com>
2021-02-08 18:18:11 -0800

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,71 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the name of your project? (Required)",
+        validate: (projectInput) => {
+          if (projectInput) {
+            return true;
+          } else {
+            console.log("Please enter your project name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username (Required)",
+        validate: (githubInput) => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log("Please enter your GitHub username!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Describe your project:",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Any necessary information needed about using this project?",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Should user be aware of anything specific before downloading?",
+      },
+      {
+        type: "input",
+        name: "contribute",
+        message:
+          "Any neccesary information needed about contributing to this project?",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Any necessary information about how to test this project?",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Choose license for your project:",
+        choices: [""],
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
